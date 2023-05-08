2b26395a83c77d7ff363f9add14379385da0179f
dr-vortex <prevett.james@gmail.com>
2023-05-08 10:48:11 -0500

    Added demo script and watch mode

--- scripts/build-demo.js
+++ scripts/build-demo.js
@@ -1,18 +1,21 @@
 import { context, build } from 'esbuild';
 
 const options = {
-	entryPoints: ['src/index.ts'],
-	outfile: 'dist/elfutils.min.js',
-	minify: true,	
+	entryPoints: ['src/demo/app.ts', 'src/demo/index.html'],
+	outdir: 'dist/demo',
 	target: ['es6'],
 	sourcemap: true,
 	bundle: true,
-	format: 'esm',
+	minify: true,
+	loader: {
+		'.html': 'copy'
+	}
 }
-
 if(['--watch', '-w'].some(flag => process.argv.includes(flag))) {
 	const ctx = await context(options);
 	await ctx.watch();
+	const { host, port } = await ctx.serve({ servedir: 'dist/demo' });
+	console.log(`Serving demo at ${host}:${port}`);
 }else{
 	await build(options);
 }
